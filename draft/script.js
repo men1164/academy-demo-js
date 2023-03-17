0199917d89a311a2a6646ef7da291ee6e938e4c4
men1164 <41180013+men1164@users.noreply.github.com>
2023-03-17 17:08:47 +0700

    for loop

---
+++ draft/script.js
@@ -0,0 +1,15 @@
+for(let i = 0; i < 5; i++) {
+  console.log(i)
+}
+
+// ! Tricky ต้องเมคชัวร์ว่าสรุปเราอยากได้ loop กี่รอบ
+for(let i = 1; i <= 5; i++) {
+  console.log(i)
+}
+
+
+const tripLocation = ['London', 'Bali', 'Amsterdam'];
+
+for (let i = 0; i < tripLocation.length; i++){
+  console.log(tripLocation[i]);
+}
