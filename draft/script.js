4cc489ffabdae4e0c9a5e1f9c575fb1d2ccfb6eb
men1164 <41180013+men1164@users.noreply.github.com>
2023-03-21 19:29:49 +0700

    promise

--- draft/script.js
+++ draft/script.js
@@ -1,14 +1,39 @@
-// Step
-// Open vscode
-// Create .html file
-// Create .js file
-// Autocomplete html
-// <script> with .js file
-// Try console.log
-// Save
-// Show at the browser
-// Show change code and refresh browser
-// Live server
-// Try change code and save
+// * Demo setTimeOut
+setTimeout(() => {
+  console.log("Yo")
+}, 3000)
 
-console.log("Hello world")
+
+// * Then promise, assume that setTimeOut is fetching data from database that takes 3 seconds to load.
+let countValue = new Promise((resolve, reject)=> {
+  setTimeout(() => {
+    if (true) {
+      // * If fetch success
+      resolve("There is a count value.");
+    } else {
+      // ! If fetch failed
+      reject("There is no count value");
+    }
+  }, 3000)
+})
+
+console.log(countValue)
+// * To show that it is parallel
+console.log("Hello")
+
+
+// * Wait for 3 seconds, it should fullfilled
+setTimeout(() => {
+  console.log(countValue)
+}, 3000)
+
+
+// * Access to returned value
+countValue
+  .then(res => {
+    console.log(res)
+  })
+  // ! Change if to false, get reject first
+  .catch(err => {
+  console.log(err)
+  })
