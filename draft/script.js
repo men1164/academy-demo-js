f10512f52488e66bfed4a6520f177ce8b14795f6
men1164 <41180013+men1164@users.noreply.github.com>
2023-03-21 17:18:47 +0700

    object

--- draft/script.js
+++ draft/script.js
@@ -1,14 +1,14 @@
-// Step
-// Open vscode
-// Create .html file
-// Create .js file
-// Autocomplete html
-// <script> with .js file
-// Try console.log
-// Save
-// Show at the browser
-// Show change code and refresh browser
-// Live server
-// Try change code and save
+const person = {
+  name: "John",
+  age: 20,
+  pet: {
+     kind: "Dog",
+     age: 2,
+  }
+}
 
-console.log("Hello world")
+console.log('Name:', person.name)
+console.log('Name:', person["age"])
+
+console.log('Pet kind:', person.pet.kind)
+console.log('Pet age:', person.pet.age)
