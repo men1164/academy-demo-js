8c51b87c7dcf4c5d50b7fcc1fc234d84fdf406b6
men1164 <41180013+men1164@users.noreply.github.com>
2023-03-21 12:48:59 +0700

    move console.log

---
+++ draft/script.js
@@ -0,0 +1,14 @@
+// Step
+// Open vscode
+// Create .html file
+// Create .js file
+// Autocomplete html
+// <script> with .js file
+// Try console.log
+// Save
+// Show at the browser
+// Show change code and refresh browser
+// Live server
+// Try change code and save
+
+console.log("Hello world")
