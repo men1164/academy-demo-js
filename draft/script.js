6e81cf71486f12b84dcb7118f5e0bb4dfa05eec6
men1164 <41180013+men1164@users.noreply.github.com>
2023-03-23 13:44:34 +0700

    async await try catch

--- draft/script.js
+++ draft/script.js
@@ -1,9 +1,3 @@
-// * Demo setTimeOut
-setTimeout(() => {
-  console.log("Yo")
-}, 3000)
-
-
 // * Then promise, assume that setTimeOut is fetching data from database that takes 3 seconds to load.
 let countValue = new Promise((resolve, reject)=> {
   setTimeout(() => {
@@ -17,26 +11,78 @@ let countValue = new Promise((resolve, reject)=> {
   }, 3000)
 })
 
-console.log(countValue)
-// * To show that it is parallel
-console.log("Hello")
+// * If there are another asynchronus tasks to run after the first one?
+let countValue2 = new Promise((resolve, reject)=> {
+  setTimeout(() => {
+    if (true) {
+      resolve("There is a count value. 2");
+    } else {
+      reject("There is no count value");
+    }
+  }, 3000)
+})
+
+let countValue3 = new Promise((resolve, reject)=> {
+  setTimeout(() => {
+    if (true) {
+      resolve("There is a count value. 3");
+    } else {
+      reject("There is no count value");
+    }
+  }, 3000)
+})
 
 
-// * Wait for 3 seconds, it should fullfilled
-setTimeout(() => {
-  console.log(countValue)
-}, 3000)
-
-
-// * Access to returned value
 countValue
   .then(res => {
     console.log(res)
+    countValue2
+      .then(res2 => {
+        console.log(res2)
+        countValue3
+          .then(res3 => {
+            console.log(res3)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   })
-  // ! Change if to false, get reject first
   .catch(err => {
     console.log(err)
   })
-  .finally(() => {
-    console.log('Finally!')
-  })
+
+
+// * Return async
+const fetchMultipleData = async () => {
+  // ! Demo try catch after
+  try {
+    const result = await countValue
+    const result2 = await countValue2
+    const result3 = await countValue3
+
+    return { result, result2, result3 }
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+fetchMultipleData().then(res => console.log(res))
+
+
+// * Non-Return async
+const fetchMultipleData2 = async () => {
+  const result = await countValue
+  const result2 = await countValue2
+  const result3 = await countValue3
+
+  console.log(result)
+  console.log(result2)
+  console.log(result3)
+}
+
+fetchMultipleData2()
