251803a301dfc4d4484394bb9cb0cf092dd6258f
men1164 <41180013+men1164@users.noreply.github.com>
2023-04-11 19:10:33 +0700

    array 2.0

--- draft/script.js
+++ draft/script.js
@@ -12,6 +12,28 @@ myArr.push(2000)
 
 console.log(myArr)
 
-// ! อย่าลืมสอนวิธีดู doc แบบเร็ว ๆ ใน vscode ด้วย
-console.log(myArr.slice(1, 3))
+
+// ! อย่าลืมสอนวิธีดู doc แบบเร็ว ๆ
+// * slice return array ใหม่ ไม่ได้แก้ array เดิมเหมือน push
+const sliced = myArr.slice(1, 3)
+
+console.log("sliced:", sliced)
+console.log("my array:", myArr)
+
 console.log(myArr.indexOf(22))
+
+
+// * ชวนดู doc แบบเร็ว ๆ อีกที
+// * ถามความแตกต่างกับ .push()
+const newArr = myArr.concat(123, true, ["another array", "john"])
+
+console.log("my array:", myArr)
+console.log("new array:", newArr)
+
+
+
+// * Exercise
+// ? Create an array that contain 4 foods that you like. Then complete these following tasks.
+// ? 1. Remove your least 2 favorite food from the array. Print it to the console.
+// ? 2. Locate your most favorite food in the array using .indexOf(). Print its array index to the console.
+// ? 3. Add your 3 favorite desserts or fruits into that array. Print the result to the console.
