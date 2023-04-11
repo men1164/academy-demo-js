f50d367734c0d9ad352f7b1a3da01a25de4c5de6
men1164 <41180013+men1164@users.noreply.github.com>
2023-04-11 12:24:21 +0700

    move undefined demo

--- draft/script.js
+++ draft/script.js
@@ -3,6 +3,10 @@ const myArr = ["Men", 22]
 console.log("My name:", myArr[0])
 console.log("My age:", myArr[1])
 
+// ! ไม่พ่น error ออกมา เป็น undefined
+console.log(myArr[10])
+
+
 myArr.push('Wed', 2000)
 
 console.log(myArr)
@@ -10,7 +14,3 @@ console.log(myArr)
 // ! อย่าลืมสอนวิธีดู doc แบบเร็ว ๆ ใน vscode ด้วย
 console.log(myArr.slice(1, 3))
 console.log(myArr.indexOf(22))
-
-
-// ! ไม่พ่น error ออกมา เป็น undefined
-console.log(myArr[10])
