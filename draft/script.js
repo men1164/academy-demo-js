b61c9e78d15bfb8a8d405028f9556acf2e35205d
men1164 <41180013+men1164@users.noreply.github.com>
2023-04-17 16:32:11 +0700

    add challenge

--- draft/script.js
+++ draft/script.js
@@ -39,3 +39,5 @@ console.log("new array:", newArr)
 // ? 1. Remove your least 2 favorite food from the array. Print it to the console.
 // ? 2. Locate your most favorite food in the array. Print its array index to the console.
 // ? 3. Add your 3 favorite desserts or fruits into that array. Print the result to the console.
+
+// ? CHALLENGES: Look at the Array methods slide. Pick one method to check if your most favorite food is existed in the array or not? (hint: return as boolean)
