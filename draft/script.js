2f51c5bb596697f68ff50a81384e79b406c140a5
men1164 <41180013+men1164@users.noreply.github.com>
2023-04-17 16:57:00 +0700

    update

--- draft/script.js
+++ draft/script.js
@@ -60,10 +60,9 @@ for(let i = 0; i < 3; i++) {
 
 
 // ? Exercise: 
-// ? Write a code that will loop from 0 to 15. For each loop, it will check if the current number is odd or even, and display a message to the console.
+// ? Write a code that will loop from 0 to 15. For each loop, it will check if the current index number is odd or even, and display a message to the console.
 
 // Example Output:
 // > 0 is even
 // > 1 is odd
 // > 2 is even
-// > 3 is odd
