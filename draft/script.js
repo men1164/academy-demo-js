0387ffbf2059440a840953003501a92385539134
men1164 <41180013+men1164@users.noreply.github.com>
2023-04-23 21:36:21 +0700

    update

--- draft/script.js
+++ draft/script.js
@@ -1,45 +1,29 @@
-// * Then promise, assume that setTimeOut is fetching data from database that takes 3 seconds to load.
-let countValue = new Promise((resolve, reject)=> {
-  setTimeout(() => {
-    if (true) {
-      // * If fetch success
-      resolve("There is a count value.");
-    } else {
-      // ! If fetch failed
-      reject("There is no count value");
-    }
-  }, 3000)
-})
+const fetchData = () => Promise.resolve("data")
 
-// * If there are another asynchronus tasks to run after the first one?
-let countValue2 = new Promise((resolve, reject)=> {
-  setTimeout(() => {
-    if (true) {
-      resolve("There is a count value. 2");
-    } else {
-      reject("There is no count value");
-    }
-  }, 3000)
-})
+const getData = async () => {
+	try {
+    const res = await fetchData()
+    console.log(res)
+  } catch (err) {
+    console.log(err)
+  }
+}
 
-let countValue3 = new Promise((resolve, reject)=> {
-  setTimeout(() => {
-    if (true) {
-      resolve("There is a count value. 3");
-    } else {
-      reject("There is no count value");
-    }
-  }, 3000)
-})
+getData()
 
 
-countValue
+// * Benefits of async await
+const data = Promise.resolve("data1")
+const data2 = Promise.resolve("data1")
+const data3 = Promise.resolve("data1")
+
+data
   .then(res => {
     console.log(res)
-    countValue2
+    data2
       .then(res2 => {
         console.log(res2)
-        countValue3
+        data3
           .then(res3 => {
             console.log(res3)
           })
@@ -56,33 +40,14 @@ countValue
   })
 
 
-// * Return async
-const fetchMultipleData = async () => {
-  // ! Demo try catch after
-  try {
-    const result = await countValue
-    const result2 = await countValue2
-    const result3 = await countValue3
-
-    return { result, result2, result3 }
-
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-fetchMultipleData().then(res => console.log(res))
-
-
-// * Non-Return async
-const fetchMultipleData2 = async () => {
-  const result = await countValue
-  const result2 = await countValue2
-  const result3 = await countValue3
+const getMultipleData = async () => {
+  const result = await data
+  const result2 = await data2
+  const result3 = await data3
 
   console.log(result)
   console.log(result2)
   console.log(result3)
 }
 
-fetchMultipleData2()
+getMultipleData()
