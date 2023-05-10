98461ef12ef6977a01cb7667dfd39cbc348f664f
men1164 <41180013+men1164@users.noreply.github.com>
2023-05-10 13:39:59 +0700

    update

--- draft/script.js
+++ draft/script.js
@@ -20,3 +20,30 @@ person2.name  = "Men"
 
 console.log(person)
 console.log(person2)
+
+
+// * Array of object: data from fetching data
+const data = [
+  {
+    name: "John",
+    age: 22,
+    job: "software engineer"
+  },
+  {
+    name: "Johny",
+    age: 26,
+    job: "web designer"
+  },
+  {
+    name: "Watson",
+    age: 36,
+    job: "marketing"
+  },
+]
+
+// * Example with array method iterators
+data.forEach((person) => {
+  console.log("Name:", person.name)
+  console.log("Age:", person.age)
+  console.log("Job:", person.job)
+})
