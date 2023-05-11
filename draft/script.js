65a93f7b70656fd31c3c3fcf3fc98557f12ef688
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-05-11 14:11:46 +0700

    separate exercise

--- draft/script.js
+++ draft/script.js
@@ -32,13 +32,3 @@ const newArr = myArr.concat(123, true, ["another array", "john"])
 
 console.log("my array:", myArr)
 console.log("new array:", newArr)
-
-
-
-// * Exercise
-// ? Create an array that contain 4 foods that you like. Then complete these following tasks.
-// ? 1. Remove your least 2 favorite food from the array. Print it to the console.
-// ? 2. Locate your most favorite food in the array. Print its array index to the console.
-// ? 3. Add your 3 favorite desserts or fruits into that array. Print the result to the console.
-
-// ? CHALLENGES: Look at the Array methods slide. Pick one method to check if your most favorite food is existed in the array or not? (hint: return as boolean)
