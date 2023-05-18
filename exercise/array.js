c5774a99d590d3514b110a8b73f8f918865a39da
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-05-18 15:46:47 +0700

    solution

--- exercise/array.js
+++ exercise/array.js
@@ -1,8 +1,17 @@
 // * Exercise
 // ? Create an array that contain 4 foods that you like. Then complete these following tasks.
 
-// ? 1. Remove your least 2 favorite food from the array. Print it to the console.
-// ? 2. Locate your most favorite food in the array. Print its array index to the console.
-// ? 3. Add your 3 favorite desserts or fruits into that array. Print the result to the console.
+// ? 1. Locate your most favorite food in the array. Print its array index to the console.
+// ? 2. Add your 3 favorite desserts or fruits into that array. Print the result to the console.
 
 // ? CHALLENGES: Look at the Array methods slide. Pick one method to check if your most favorite food is existed in the array or not? (hint: return as boolean)
+
+
+const myFavFood = ["TomYum", "SomTum", "Salmon", "Pasta"]
+
+console.log(myFavFood.indexOf("Salmon"))
+
+myFavFood.push("Ice-cream", "Yogurt", "Orange")
+console.log(myFavFood)
+
+console.log(myFavFood.includes("Salmon"))
